d8de2d8f2444c3e5d3aaf3579cf3f56c0527a9b8
Dirk de la Hunt <dirk.delahunt@embark-studios.com>
2021-03-30 20:36:02 +0200

    First workingish version.
    TODO:
    Need to figure out how to measure fast moving things
    Need to make distance configurable

---
+++ Arduino/Chroduino.ino
@@ -0,0 +1,215 @@
+// Chroduino, an almost functional chronograph by Dirk de la Hunt 2021
+// ================== CONFIG ==================== //
+
+// Feature toggle
+#define FEATURE_SCREEN   // SSD1306 OLED screen toggle
+#define FEATURE_BLE      // Bluetooth Low Energy toggle
+#define FEATURE_SERIAL   // serial output toggle
+
+// Timing values
+#define MEASUREMENT_TIMEOUT_MS   1000   // how long before a measurement times out (milliseconds)
+#define DUPLICATE_EPSILON_MPS    0.01   // the smallest difference between readings to be considered unique (meters per second)
+//TODO: MEASUREMENT_DISTANCE_MM, it measures 100mm right now, but make it configurable
+
+// These are the pins the IR LEDs are connected to.
+// NOTE: these have to be interrupt pins. If you're unsure, use pin 2 and 3.
+// (see the pins for your board here: https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/)
+#define SENSORPIN_1   2
+#define SENSORPIN_2   3
+
+// ================== CODE ====================== //
+
+#ifdef FEATURE_SCREEN
+  #include <SPI.h>
+  #include <Wire.h>
+  #include <Adafruit_GFX.h>
+  #include <Adafruit_SSD1306.h>
+
+  #define SCREEN_WIDTH   128
+  #define SCREEN_HEIGHT  32
+  #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+  
+  Adafruit_SSD1306 g_display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#endif
+
+#ifdef FEATURE_BLE
+  #include <ArduinoBLE.h>
+  
+  BLEService             g_chrono_service("19B10010-E8F2-537E-4F6C-D104768A1213");
+  BLEFloatCharacteristic g_speed_characteristic("19B10012-E8F2-537E-4F6C-D104768A1213", BLERead | BLENotify);
+#endif
+ 
+volatile unsigned long g_us1           = 0;   // sensor 1 time (microseconds)
+volatile unsigned long g_us2           = 0;   // sensor 2 time (microseconds)
+long                   g_start_ms      = 0;   // first time any sensor got a reading, used for timeouts (millisecond)
+unsigned int           g_count         = 0;   // how many successful measurements taken so far
+float                  g_running_total = 0.0; // the running total (meters per second)
+float                  g_previous      = 0.0; // the previous reading, used for duplicates (meters per second)
+
+#ifdef FEATURE_SCREEN
+void initFEATURE_SCREEN()
+{
+#if SCREEN_HEIGHT == 64  
+  if(!g_display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) // Address 0x3D for 128x64
+#elif SCREEN_HEIGHT == 32
+  if(!g_display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) // Address 0x3C for 128x32
+#else
+  #error Unknown screen resolution, can't determine default address
+#endif    
+  {
+    Serial.println(F("SSD1306 OLED allocation failed"));
+    while(1);
+  }
+  g_display.clearDisplay();
+  g_display.setTextSize(1);
+  g_display.setTextColor(SSD1306_WHITE);
+  g_display.setCursor(0, 0);
+  g_display.println("Waiting...");
+  g_display.display();    
+}
+#endif    
+
+#ifdef FEATURE_BLE
+void initFEATURE_BLE()
+{
+  if (!BLE.begin())
+  {
+    Serial.println("starting BLE failed!");
+    #ifdef FEATURE_SCREEN
+      g_display.setCursor(0,0);
+      g_display.println("Could not start BLE");
+      g_display.display();    
+    #endif
+    //while(1); // not the end of the world?
+  }
+
+  BLE.setLocalName("Chroduino");
+  BLE.setAdvertisedService(g_chrono_service);
+
+  g_chrono_service.addCharacteristic(g_speed_characteristic);
+
+  BLE.addService(g_chrono_service);
+
+  g_speed_characteristic.writeValue(0.0);
+
+  BLE.advertise();
+}
+#endif
+
+void update_sensor1()
+{
+  if (!g_us1) // ignore duplicate readings
+    g_us1 = micros();
+}
+
+void update_sensor2()
+{
+  if (!g_us2) // ignore duplicate readings
+    g_us2 = micros();
+}
+
+void setup()
+{
+#ifdef FEATURE_SERIAL
+  Serial.begin(115200);
+  Serial.println("Hi");
+#endif  
+  
+  // initialize the sensor pins as an input:
+  pinMode(SENSORPIN_1, INPUT);     
+  digitalWrite(SENSORPIN_1, HIGH); // turn on the pullup
+  pinMode(SENSORPIN_2, INPUT);     
+  digitalWrite(SENSORPIN_2, HIGH); // turn on the pullup
+
+  attachInterrupt(digitalPinToInterrupt(SENSORPIN_1), update_sensor1, LOW); //maybe CHANGE instead?
+  attachInterrupt(digitalPinToInterrupt(SENSORPIN_2), update_sensor2, LOW);
+
+#ifdef FEATURE_SCREEN
+  // initialiaze the screen first, so we can print error messages on it
+  initFEATURE_SCREEN();
+#endif
+#ifdef FEATURE_BLE
+  initFEATURE_BLE();
+#endif
+}
+
+void reset_timers()
+{
+  g_start_ms = 0;
+  g_us1      = 0;
+  g_us2      = 0;
+}
+ 
+void loop()
+{
+  unsigned long now = millis();
+  
+#ifdef FEATURE_BLE
+  BLE.poll();
+#endif
+  
+  if (g_us1 || g_us2)
+  {
+    if (!g_start_ms)
+      g_start_ms = now;
+
+    if (now - g_start_ms > MEASUREMENT_TIMEOUT_MS) // timeout for both beams to be broken
+    {
+      reset_timers();
+#ifdef FEATURE_SERIAL      
+      Serial.println("ERROR: Only got 1 trigger, aborting...");
+#endif      
+#ifdef FEATURE_SCREEN
+      {
+        g_display.clearDisplay();
+        g_display.setCursor(0,0);               // Start at top-left corner
+        g_display.println("*** ERROR ***");
+      }
+#endif          
+      return;
+    }
+
+    if (g_us1 && g_us2) 
+    {
+      int           duplicate          = g_count++; // shot 0 can't be a duplicate
+      unsigned long trigger_difference = (g_us1 - g_us2);
+      float         speed_mps          = (100000000 / ((float)trigger_difference * 1000.0)); // meters per second
+      
+#ifdef FEATURE_SERIAL      
+      Serial.println("Results:");
+      Serial.print("Trigger difference (microsecond): ");
+      Serial.println(trigger_difference);
+      Serial.print("meters/second: ");
+      Serial.println(speed_mps);
+#endif
+
+      g_running_total += speed_mps;
+      duplicate        = duplicate && ( fabs(speed_mps - g_previous) < DUPLICATE_EPSILON_MPS );
+      g_previous       = speed_mps;
+
+#ifdef FEATURE_SCREEN
+      {
+        g_display.clearDisplay();
+        g_display.setCursor(0,0);
+        g_display.print("speed: ");
+        g_display.print(speed_mps);
+        g_display.println(" m/s");
+        if (duplicate)
+          g_display.println("(duplicate)");
+        g_display.print("average: ");
+        g_display.print(g_running_total / (float)g_count);
+        g_display.println(" m/s");
+        g_display.print("count: ");
+        g_display.println(g_count);
+        g_display.display();
+      }
+#endif        
+#ifdef FEATURE_BLE
+      g_speed_characteristic.writeValue(speed_mps);
+#endif  
+
+      reset_timers();
+      delay(500); // take a break buddy, you earned it.
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # Chroduino
-Arduino-based chronograph with OLED and BLE support
+
+This is an Arduino-based chronograph, with I2C OLED (screen) and BLE (Bluetooth, low energy) support.
+
+![OLED demo](/images/oled.jpg)
+
+**NOTE**:
+ * work in progress, doesn't seem to work with fast moving things, like BBs or pellets.
+ * at the moment, the distance between IR sensors is fixed at 10cm
+ * configuration is mostly done by changing `define` values at the top of the Arduino file
+
+### Wiring
+
+This projects uses the IR Break Beam Sensors from [here](https://www.adafruit.com/product/2168), making it easy to wire up.
+* all the red wires go to 3v or 5v
+* all the black wires go to ground (`GND`)
+* one white wire connects to pin 2, the other connects to pin 3 (or any other pins that support interrupts, just be sure to modify `SENSORPIN_1` and `SENSORPIN_2` in the Arduino code)
+* depending on your board, you can power it with a 9v battery by connecting the positive terminal to `VIN`, and the negative terminal to `GND`
+
+**NOTE**:
+ * this uses interrupts for the sensors, so make sure you connect them to pins that support it (pins 2 and 3 should work, if you're not sure)
+ * for the I2C OLED screens to automagically work, connect them to pins 4 (`SDA`) and 5 (`SCL`)
+
+### OLED view
+
+Support for `SSD1306` devices exists, catering for 128x64 and 128x32 screens out of the box, by making sure `#define FEATURE_SCREEN` is enabled at the top of the Arduino file.
+Specifying the resolution of your screen determines what address to find the screen on (`0x3C` or `0x3C` respectively).
+
+**NOTE**:
+ * these screens normally need `SDA` and `SCL` plugged into pins 4 and 5 respectively.
+
+### BLE
+
+If you have an e.g., Arduino Nano BLE 33, then you can enable it by making sure `#define FEATURE_BLE` is enabled at the top of the Arduino file.
+It will advertise itself as a Bluetooth device named `Chroduino` to which you can pair with, and subscribe to be notified when its single float value changes (the last measured time).
+
+##### BLE web view
+
+A very barebones viewer of the speed data using BLE is provided in the `Web` folder.
+**NOTE:**: this *requires*
+ * HTTPS or binding explicitly to localhost for debugging
+    * I use Python 3 as my test server, with `python -m http.server --bind localhost`
+ * UUID's to be in lowercase
+ * using Chrome or Edge to view (see [here](https://developer.mozilla.org/en-US/docs/Web/API/Web_Bluetooth_API) for browser compatibility)
+    * using my Python server commandline above, browse to `http://localhost:8000/ble_notify.html`
+
+When you first click `Start`, you will get a dialog prompting you which device you'd like to pair with, select `Chroduino`.
+
+![Pairing](/images/pairing.jpg)
+
+With every measurement taken, a new line will be added to the list, which can be erased by click the `Clear` button.
+If you find you're getting weird values, check the endianness of the float value.
+
+![Measuring](/images/measuring.jpg)
